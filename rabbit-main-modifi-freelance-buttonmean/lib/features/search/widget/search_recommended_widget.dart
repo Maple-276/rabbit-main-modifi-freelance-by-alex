cc87298dd1fe03b7d9f6c99e56118609c20d76b9
Alexander Portillo <167712786+Maple-276@users.noreply.github.com>
2025-04-12 01:12:34 -0500

    Funciono en la laptop cambio de un opacity en widget

--- rabbit-main-modifi-freelance-buttonmean/lib/features/search/widget/search_recommended_widget.dart
+++ rabbit-main-modifi-freelance-buttonmean/lib/features/search/widget/search_recommended_widget.dart
@@ -273,7 +273,8 @@ class _RecommendedCuisinesShimmerWidget extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Shimmer(
             interval: const Duration(seconds: 1),
-            color: Theme.of(context).shadowColor.withOpacity(0.2),
+            // color: Theme.of(context).shadowColor.withOpacity(0.05),
+            colorOpacity: 0.1,
             enabled: true,
             child: Container(
               height: 20.0,
@@ -291,7 +292,7 @@ class _RecommendedCuisinesShimmerWidget extends StatelessWidget {
           child: Shimmer(
             duration: const Duration(seconds: 2),
             interval: const Duration(seconds: 1),
-            color: Colors.grey.withOpacity(0.1),
+            colorOpacity: 0.1,
             enabled: true,
             child: Container(
               margin: const EdgeInsets.symmetric(horizontal: Dimensions.paddingSizeExtraSmall),
@@ -322,6 +323,7 @@ class _RecommendedCategoryShimmerWidget extends StatelessWidget {
         Shimmer(
           interval: const Duration(seconds: 1), // Delay between shimmers
           color: Theme.of(context).shadowColor.withOpacity(0.2), // Base color
+          colorOpacity: 0.1, // Opacity of shimmer
           enabled: true, // Enable shimmer effect
           child: Container(
             height: 20,
@@ -345,7 +347,8 @@ class _RecommendedCategoryShimmerWidget extends StatelessWidget {
           itemBuilder: (context, index) => Shimmer(
             duration: const Duration(seconds: 2),
             interval: const Duration(milliseconds: 300),
-            color: Colors.grey.withOpacity(0.5),
+            color: Theme.of(context).shadowColor.withOpacity(0.2),
+            colorOpacity: 0.5,
             enabled: true,
             child: Container(
               decoration: BoxDecoration(
@@ -379,3 +382,4 @@ class _RecommendedCategoryShimmerWidget extends StatelessWidget {
     );
   }
 }
+
